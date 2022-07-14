d63867b89df5ae154e39b7e61f6d540bf009e7f0
jkoufalas <john@koufalas.com>
2022-07-14 21:10:23 +0930

    changing branches

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,16 @@
 // This is the javascript file
-var mainScreenDOM = document.querySelector("#mainScreen");
-var mainScreenDOMJQ = $('#mainScreen');
+var mainScreenDOM = document.querySelector("#outerClass");
+var mainScreenDOMJQ = $('#outerClass');
 
-var searchNewReleaseList = document.querySelector("#newRelease");
+var searchNewReleaseList = $('#nowShowing');
 
 
 
 function buttonClickNewRelease(event) {
     var appID = 'edae2dbf4933f27205a897a516b34101';
     var apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='+ appID;
+    console.log('Got Here');
   
-  //get city lat long from name
   fetch(apiUrl)
   .then(function (response) {
     return response.json();
@@ -19,14 +19,35 @@ function buttonClickNewRelease(event) {
     console.log(data);
     mainScreenDOMJQ.empty();
     for( var i = 0 ; i < data.results.length; i++){
-        var containerList = document.createElement("img");
+        var outerLayer = document.createElement("div");
         var posterPath = 'https://image.tmdb.org/t/p/w500'+data.results[i].poster_path;
-        containerList.setAttribute('src', posterPath);
-        mainScreenDOM.appendChild(containerList);
+        outerLayer.classList.add('columns', 'is-multiline');
+
+        var cardLayer = document.createElement("div");
+        cardLayer.classList.add('column','is-one-quarter-desktop','is-half-tablet');
+
+        var cardLayerInner = document.createElement("div");
+        cardLayerInner.classList.add('card');
+
+        var cardImage = document.createElement("div");
+        cardImage.classList.add('card-image');
+
+        var cardFig = document.createElement("figure");
+        cardFig.classList.add('image','is-3by2');
+
+        var cardImg = document.createElement("img");
+        cardImg.setAttribute('src', posterPath);
+
+
+        cardImg.appendChild(cardFig);
+        cardLayerInner.appendChild(cardImage);
+        cardLayer.appendChild(cardLayerInner);
+        outerLayer.appendChild(cardLayer);
+        mainScreenDOM.appendChild(outerLayer);
     }
   });
-}
+};
 
 
 
-searchNewReleaseList.addEventListener('click', buttonClickNewRelease);
+searchNewReleaseList.on('click', buttonClickNewRelease);
