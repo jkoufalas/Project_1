9363c190dc18ce82c015f302c25c4b20e4e39f59
jkoufalas <john@koufalas.com>
2022-07-19 19:28:48 +0930

    Added Faourite test

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,8 +7,17 @@ var searchPopularList = $('#Popular');
 var searchTopRatedList = $('#TopRated');
 var searchList = $('#search');
 var favouritesList = $('#favourites');
-var openMovieModal = document.querySelector("#ex1");
-var bodyLoc = document.querySelector("body");
+var openMovieModal = $('#ex1');
+
+var modalMovieImageItm = document.querySelector("#modalMovieImage");
+var modalMovieTitleItm = document.querySelector("#modalMovieTitle");
+var modalMovieDescItm = document.querySelector("#modalMovieDesc");
+var modalMovieGenreItm = document.querySelector("#modalMovieGenre");
+var modalMoviePopItm = document.querySelector("#modalMoviePop");
+var addToFavouritesItm = $('#addToFavourites');
+
+
+
 
 
 
@@ -69,6 +78,16 @@ fetch(apiUrl)
 });
 };
 
+function addMovieToFavourites(event) {
+  console.log("data");
+
+  var appID = 'edae2dbf4933f27205a897a516b34101';
+  //var pageNum = 1;
+  var apiUrl = 'https://api.themoviedb.org/3/movie/top_rated?api_key='+ appID + '&language=en-US&page='+ pageNum;
+  
+
+};
+
 function buttonSearch(event, title) {
 
   var appID = 'edae2dbf4933f27205a897a516b34101';
@@ -87,23 +106,45 @@ fetch(apiUrl)
 function buttonOpenModal(event) {
   var element = event.target;
   var movieNum = element.getAttribute("movie-id");
-  console.log("button Pressed - "+movieNum);
 
-  openMovieModal.setAttribute("style", "display: inline-block");
-  bodyLoc.setAttribute("style", "display: inline-block; overflow: hidden");
-  /*
+  
+  
   var appID = 'edae2dbf4933f27205a897a516b34101';
-  var pageNum = 1;
-  var apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key='+ appID + '&language=en-US&page='+ pageNum + '&query=' + title;
+  var apiUrl = 'https://api.themoviedb.org/3/movie/'+ movieNum +'?api_key='+ appID + '&language=en-US';
 
 fetch(apiUrl)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
-  displayMovieList(data);
+  console.log(data);
+  var posterPath = 'https://image.tmdb.org/t/p/w500'+data.poster_path;
+  modalMovieImageItm.setAttribute('src', posterPath);
+
+  modalMovieTitleItm.textContent = data.title;
+  modalMovieDescItm.textContent = data.overview;
+  var genreArr = [];
+  for(var i = 0;i<data.genres.length;i++){
+    genreArr.push(data.genres[i].name);
+  }
+  var genres = genreArr.join(", ");
+  modalMovieGenreItm.textContent = genres;
+  modalMoviePopItm.textContent = data.vote_average;
+
+if(favoriteList.includes(parseInt(movieNum,10))){
+  console.log("In Favourites");
+  //addToFavouritesItm.html("Remove from Favourites");
+}else{
+  console.log("Not In Favourites");
+  //addToFavouritesItm.text("Add to Favourites");
+}
+  
+  
+
+  //console.log(data);
+  openMovieModal.modal('open');
 });
-*/
+
 };
 
 function buttonSearchFavourites(event) {
@@ -112,7 +153,7 @@ function buttonSearchFavourites(event) {
   for( var i = 0 ; i < favoriteList.length; i++){
     var appID = 'edae2dbf4933f27205a897a516b34101';
     var apiUrl = 'https://api.themoviedb.org/3/movie/' + favoriteList[i] + '?api_key='+ appID + '&language=en-US';
-  
+
     fetch(apiUrl)
     .then(function (response) {
       return response.json();
@@ -139,6 +180,7 @@ function buttonSearchFavourites(event) {
       var cardImg = document.createElement("img");
       cardImg.setAttribute('src', posterPath);
       cardImg.setAttribute('movie-id', data.id);
+      addToFavouritesItm.attr("movie-id",data.id);
 
 
       cardFig.appendChild(cardImg);
@@ -186,6 +228,7 @@ var displayMovieList = function (data){
         var cardImg = document.createElement("img");
         cardImg.setAttribute('src', posterPath);
         cardImg.setAttribute('movie-id', data.results[i].id);
+        addToFavouritesItm.attr("movie-id",data.id);
 
         cardFig.appendChild(cardImg);
         cardImage.appendChild(cardFig);
@@ -206,3 +249,5 @@ searchTopRatedList.on('click', event => buttonClickTopRated(event, 1));
 searchList.on('click', event => buttonSearch(event, "minion"));
 favouritesList.on('click', buttonSearchFavourites);
 mainScreenDOMJQ.on('click', buttonOpenModal);
+
+addToFavouritesItm.on('click', addMovieToFavourites);
